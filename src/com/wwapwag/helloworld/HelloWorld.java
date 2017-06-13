2ac75647c6a5183ac94f3db0832262326df0cdfe
sdwbvios <jiangzehu@163.com>
2017-06-13 17:14:20 +0800

    Initial commit

---
+++ src/com/wwapwag/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.wwapwag.helloworld;
+
+/**
+ * Created by jiangzh on 2017/6/13.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
