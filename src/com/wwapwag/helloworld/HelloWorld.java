6b2695d286904bd283c5ace5b624e5a46a1b4168
sdwbvios <jiangzehu@163.com>
2017-06-14 08:39:29 +0800

    112312

--- src/com/wwapwag/helloworld/HelloWorld.java
+++ src/com/wwapwag/helloworld/HelloWorld.java
@@ -7,5 +7,10 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        test();
+    }
+
+    public static void test(){
+        System.out.println("");
     }
 }
