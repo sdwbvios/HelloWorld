a7eb4afbe666b7500b5f0b1a338a87f2c0fecfbf
sdwbvios <jiangzehu@163.com>
2017-06-14 10:35:26 +0800

    qq

---
+++ src/com/wwapwag/helloworld/test.java
@@ -0,0 +1,10 @@
+package com.wwapwag.helloworld;
+
+/**
+ * Created by jiangzh on 2017/6/14.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("lalala");
+    }
+}
